a915ed5397a5d804d9ba3ae413bc72ace866299d
Cesar Villanueva <lordchief21@gmail.com>
2023-04-18 02:32:17 -0600

    "Create projects Cards"

--- src/components/projects/Projects.jsx
+++ src/components/projects/Projects.jsx
@@ -1,15 +1,35 @@
 import React from "react";
+import style from './style.css'
+import all_foods from '../../assets/all_foods.jpg'
+import fastAPI from '../../assets/fastAPI.jpg'
+import Unkwon_coder from '../../assets/Unkwon_coder.jpg'
 
 export const Projects = () => {
     return (
         <section id = "Projects" >
-            <h2>Portfolio</h2>
+            <h2 className="title_project">Portfolio</h2>
 
             <div className="container portfolio_container">
-                <article>
-                    <div>Image of Project</div>
+                <article className="card">
+                    <div>
+                        <img className="img_project" src={fastAPI} alt="all_foods_project"/>
+                    </div>
                     <h3>API using FastAPI (Python Integration)</h3>
                     <a href="https://github.com/lordchief21/Back-End_FastAPI-Python" className="bg-green-700 rounded-full scroll-pl-6">Github</a>
+                </article>
+                <article className="card">
+                    <div>
+                        <img className="img_project" src={Unkwon_coder} alt="all_foods_project"/>
+                    </div>
+                    <h3>Uknown coder (PG-HENRY)</h3>
+                    <a href="https://github.com/lordchief21/PG-SoyHenry" className="bg-green-700 rounded-full scroll-pl-6">Github</a>
+                </article>
+                <article className="card">
+                    <div>
+                        <img className="img_project" src={all_foods} alt="all_foods_project"/>
+                    </div>
+                    <h3>Food_PI (PI-HENRY)</h3>
+                    <a href="https://github.com/lordchief21/Food_PI" className="bg-green-700 rounded-full scroll-pl-6">Github</a>
                 </article>
             </div>
 
